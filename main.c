c1b300154011879d816e85de1a9e8a080dc8300d
Nahi Bataunga <126324992+protectorOfPotatoes@users.noreply.github.com>
2023-03-24 22:13:54 +0530

    main.c
    STATUS: INCOMPLETE (24.3.23/Friday)
    Tic tac toe code

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define input(buffer) fgets(buffer, sizeof(buffer), stdin); 
+
+typedef struct board
+{
+	char position[3][3] = { {'0','0','0'},
+							{'0','0','0'},
+							{'0','0','0'}
+						  };
+}board_t;
+
+int main() {
+	
+	return 0;
+}
